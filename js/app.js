c7e726ea74d4b385b39631b1b8a1486efb82aa6e
Alexandr <alexandr.d.walker@gmail.com>
2023-08-03 23:40:42 +0700

    create mobile menu

--- js/app.js
+++ js/app.js
@@ -1,56 +1,47 @@
 $(function () {
-    let header = $("#header");
-    let intro = $("#intro");
-    let introH = intro.innerHeight();
-    let scrollPos = $(window).scrollTop();
-    let nav = $("#nav");
-    let navToggle = $("#navToggle");
+  let header = $("#header");
+  let intro = $("#intro");
+  let nav = $("#nav");
+  let mobMenu = $("#mobMenu");
+  let burger = $("#burger");
 
-		var scrollPrev = 0;
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
 
-    $(window).scroll(function() {
-	    var scrolled = $(window).scrollTop();
- 
-	    if ( scrolled > 100 && scrolled > scrollPrev ) {
-		    header.addClass("out");
-	    } else {
-		    header.removeClass("out");
-	    }
-	    scrollPrev = scrolled;
-    });
-  
-    /* Smooth Scroll*/
-    $("[data-scroll]").on("click", function (event) {
-      event.preventDefault();
-  
-      let elementId = $(this).data("scroll");
-      let elementOffset = $(elementId).offset().top;
-  
-      nav.removeClass("show");
-  
-      $("html, body").animate(
-        {
-          scrollTop: elementOffset - 70,
-        },
-        700
-      );
-    });
-  
-    /* NavToggle */
-    navToggle.on("click", function (event) {
-      event.preventDefault();
-      nav.toggleClass("show");
-    });
-  
-    /* Reviews: https://kenwheeler.github.io/slick/ */
-    let slider = $("#reviewsSlider");
-  
-    slider.slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      fade: true,
-      arrows: false,
-      dots: true,
-    });
+  var scrollPrev = 0;
+
+  $(window).scroll(function () {
+    var scrolled = $(window).scrollTop();
+
+    if (scrolled > 100 && scrolled > scrollPrev) {
+      header.addClass("out");
+    } else {
+      header.removeClass("out");
+    }
+    scrollPrev = scrolled;
   });
+
+  /* Smooth Scroll*/
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+
+  /* NavToggle */
+  burger.on("click", function (event) {
+    event.preventDefault();
+    burger.toggleClass("active");
+    mobMenu.toggleClass("show");
+  });
+});
